15e0d6b18e7b3248549cbac3137a67c6c6f56159
Matheus <32282486+ImMatsu@users.noreply.github.com>
2019-08-10 20:04:28 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var T = new Twit(config);
 tweetIt();
 setInterval(tweetIt,1000*60*60);//tweet vai ocorrer a cada 1 hora 1000*60*60
 function tweetIt(){
-    var r=Math.floor(Math.random()*86);//Here you must enter the number of elements in list r
+    var r=Math.floor(Math.random()*86);//Here you must enter the number of elements in list 
     var list=[]//the tweets the bot will make should be written here
     var tweet={
         status: list[r]
