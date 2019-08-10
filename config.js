784246a4e9270f1412fda0bec9cca5fdb7ae672d
Matheus <32282486+ImMatsu@users.noreply.github.com>
2019-08-10 19:58:17 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
-    consumer_key:         'XJ9ctfzjM8UhfOGVzeABnsmzM',
-    consumer_secret:      'FQodEl6vsVHOWThHJdJe6en2IyaE0PbhDp1UTS4I6nmgNCmFmt',
-    access_token:         '1158914279514562561-ID3n3lCzwQZRnwEd685wibrjN9OK50',
-    access_token_secret:  'HHHqROf8zmrzGsyEfhsrLg7ZH6gbL5aSUf8xJXyqVzegt',
+    consumer_key:         '......',
+    consumer_secret:      '......',
+    access_token:         '......',
+    access_token_secret:  '......',
 }
